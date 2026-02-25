03b4b6f888c5a4e40f95cad6e2d3496f81fd722c
imprajit <prajitr24@gmail.com>
2026-02-25 20:47:56 +0530

    Add One.py

---
+++ one.py
@@ -0,0 +1,10 @@
+a=8 
+b=3
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor Division:",a//b) 
+print("Modulo:",a%b)
+print("Power:",a**b)
+input("Press enter to exit")
