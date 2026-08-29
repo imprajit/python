eed48d3fc1862cfe92853ada32e285598a6502e7
imprajit <prajitr24@gmail.com>
2026-08-29 15:20:34 +0530

    also add address

---
+++ alsoaddress.py
@@ -0,0 +1,6 @@
+name=str(input("Enter your name:"))
+age=int(input("Enter your age:"))
+address=str(input("Enter your address:"))
+print("Hi",name)
+print("Your age is",age)
+print("Your address is",address)
