c2a566ad951722015f9d296837ade46fc3ca7bee
imprajit <prajitr24@gmail.com>
2026-08-29 16:04:31 +0530

    Will RCB win next year ?

---
+++ rcb.py
@@ -0,0 +1,8 @@
+print("Will RCB win this year")
+rcb = str(input("(Win/lose)")).lower()  # Convert the input to lowercase to make the comparison case insensitive
+if rcb == "win":
+    print("Good job")
+elif rcb == "lose":
+    print("Yeah god is good!!!")
+else:
+    print("Bro type win or lose!")
